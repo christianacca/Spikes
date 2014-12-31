3cc6c2f59b53bc34ac61a7cd27c24e27c19bda59
christianacca <ghp@christianacca.e4ward.com>
2014-12-31 13:18:52 +0000

    Fixed migration batch file

--- src/Spikes.Migrations.Tests/run_db_migrations.bat
+++ src/Spikes.Migrations.Tests/run_db_migrations.bat
@@ -3,6 +3,6 @@ SET CurrentPath=%CD%
 SET ConfigFile=%CurrentPath%\bin\Debug\Spikes.Migrations.Tests.dll.config
 SET MigrateExe=..\..\lib\packages\EntityFramework.6.1.1\tools\migrate.exe
 
-%MigrateExe% Spikes.Migrations.BaseData.dll /StartUpDirectory:%CurrentPath%\bin\Debug\ /startUpConfigurationFile:"%ConfigFile%"
-%MigrateExe% Spikes.Migrations.Data.dll /StartUpDirectory:%CurrentPath%\bin\Debug\ /startUpConfigurationFile:"%ConfigFile%"
+%MigrateExe% Spikes.Migrations.BaseDataMigrations.dll /StartUpDirectory:%CurrentPath%\bin\Debug\ /startUpConfigurationFile:"%ConfigFile%"
+%MigrateExe% Spikes.Migrations.DataMigrations.dll /StartUpDirectory:%CurrentPath%\bin\Debug\ /startUpConfigurationFile:"%ConfigFile%"
 PAUSE
