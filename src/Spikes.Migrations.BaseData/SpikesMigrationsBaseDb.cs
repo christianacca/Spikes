4d526e10b932cd2f3a763b801c5e714d85d22ff9
christianacca <ghp@christianacca.e4ward.com>
2014-12-31 09:21:08 +0000

    Code comment

--- src/Spikes.Migrations.BaseData/SpikesMigrationsBaseDb.cs
+++ src/Spikes.Migrations.BaseData/SpikesMigrationsBaseDb.cs
@@ -27,11 +27,14 @@ namespace Spikes.Migrations.BaseData
             modelBuilder.Types()
                 .Where(t => t.Assembly == baseModelAssembly)
                 .Configure(c => c.ToTable(new EnglishPluralizationService().Pluralize(c.ClrType.Name), "Base"));
+            modelBuilder.HasDefaultSchema("Base");
+
+            // note: this is a workaround to the standard way of mapping Table-per-hierarchy mapping for UserRole
+            // we're doing this so that the single table get's created in the schema we want all tables
             var userRoleType = typeof (UserRole);
             modelBuilder.Types()
                 .Where(t => userRoleType.IsAssignableFrom(t))
                 .Configure(c => c.ToTable(new EnglishPluralizationService().Pluralize(userRoleType.Name), "Base"));
-            modelBuilder.HasDefaultSchema("Base");
         }
     }
 }
