7af8b94e874237ce19d0a2864fe21658d50f402f
christianacca <ghp@christianacca.e4ward.com>
2014-12-31 14:05:03 +0000

    Ensure that udf's are not included in migrations

--- src/Spikes.Migrations.DataMigrations/SpikesMigrationsDb.cs
+++ src/Spikes.Migrations.DataMigrations/SpikesMigrationsDb.cs
@@ -1,7 +1,23 @@
-﻿namespace Spikes.Migrations.DataMigrations
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Reflection;
+using Spikes.Migrations.BaseModel;
+
+namespace Spikes.Migrations.DataMigrations
 {
     public class SpikesMigrationsDb : Data.SpikesMigrationsDb
     {
-         
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Types()
+                .Configure(c =>
+                {
+                    var udfProperties =
+                        c.ClrType.GetProperties().Where(p => p.GetCustomAttribute<UdfAttribute>() != null).ToList();
+                    udfProperties.ForEach(p => c.Ignore(p));
+                });
+        }
     }
 }
