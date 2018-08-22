b24e87356b8fbe07fd1b81857f5bf3ff27505e9f
christianacca <christian.crowhurst@gmail.com>
2018-08-22 13:55:43 +0100

    Uri examples

---
+++ src/Spikes.DotNet/Classes/UriBuilderExamples.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Eca.Spikes.DotNet.Classes
+{
+    [TestFixture]
+    public class UriBuilderExamples_Path
+    {
+        [Test]
+        public void Leading_slash_not_required()
+        {
+            var existingTrailingSlash = new Uri("http://somedomain/");
+            var value = new UriBuilder(existingTrailingSlash)
+            {
+                Path = "Spa"
+            };
+
+            Assert.That(value.Uri.ToString(), Is.EqualTo("http://somedomain/Spa"));
+        }
+
+        [Test]
+        public void Leading_slash_ignored()
+        {
+            var existingTrailingSlash = new Uri("http://somedomain/");
+            var value = new UriBuilder(existingTrailingSlash)
+            {
+                Path = "/Spa"
+            };
+
+            Assert.That(value.Uri.ToString(), Is.EqualTo("http://somedomain/Spa"));
+        }
+
+        [Test]
+        public void Trailing_slash_preserved()
+        {
+            var existingTrailingSlash = new Uri("http://somedomain/");
+            var value = new UriBuilder(existingTrailingSlash)
+            {
+                Path = "Spa/"
+            };
+
+            Assert.That(value.Uri.ToString(), Is.EqualTo("http://somedomain/Spa/"));
+        }
+
+        [Test]
+        public void Trailing_slash_not_added_to_path_by_default()
+        {
+            var existingTrailingSlash = new Uri("http://somedomain/");
+            var value = new UriBuilder(existingTrailingSlash)
+            {
+                Path = "Spa"
+            };
+
+            Assert.That(value.Uri.ToString(), Is.EqualTo("http://somedomain/Spa"));
+        }
+
+        [Test]
+        public void Trailing_slash_will_not_be_normalized()
+        {
+            var existingTrailingSlash = new Uri("http://somedomain/");
+            var value = new UriBuilder(existingTrailingSlash)
+            {
+                Path = "Spa//"
+            };
+
+            Assert.That(value.Uri.ToString(), Is.EqualTo("http://somedomain/Spa//"));
+        }
+    }
+
+    [TestFixture]
+    public class UriBuilderExamples_Fragment
+    {
+        [Test]
+        public void Hash_character_not_supplied()
+        {
+            var value = new UriBuilder("http://somedomain/Spa")
+            {
+                Fragment = "assets"
+            };
+
+            Assert.That(value.Uri.ToString(), Is.EqualTo("http://somedomain/Spa#assets"));
+        }
+
+        [Test]
+        public void Url_trailing_slash_will_be_preserved()
+        {
+            var value = new UriBuilder("http://somedomain/Spa/")
+            {
+                Fragment = "assets/1"
+            };
+
+            Assert.That(value.Uri.ToString(), Is.EqualTo("http://somedomain/Spa/#assets/1"));
+        }
+
+        [Test]
+        public void Fragment_leading_slash_will_be_preserved()
+        {
+            var value = new UriBuilder("http://somedomain/Spa/")
+            {
+                Fragment = "/assets/1"
+            };
+
+            Assert.That(value.Uri.ToString(), Is.EqualTo("http://somedomain/Spa/#/assets/1"));
+        }
+    }
+}
---
+++ src/Spikes.DotNet/Classes/UriExamples.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Eca.Spikes.DotNet.Classes
+{
+    [TestFixture]
+    public class UriExamples_Path
+    {
+        [Test]
+        public void Will_automatically_add_empty_path()
+        {
+            var value = new Uri("http://somedomain");
+            Assert.That(value.ToString(), Is.EqualTo("http://somedomain/"));
+        }
+
+        [Test]
+        public void Will_not_add_trailing_slash_to_path()
+        {
+            var value = new Uri("http://somedomain/Spa");
+            Assert.That(value.ToString(), Is.EqualTo("http://somedomain/Spa"));
+        }
+    }
+
+    [TestFixture]
+    public class UriExamples_equals
+    {
+        public void Uses_value_based_equality()
+        {
+            var left = new Uri("http://somedomain");
+            var right = new Uri("http://somedomain");
+            var different = new Uri("http://otherdomain");
+
+            Assert.That(left == right, Is.True);
+            Assert.That(left == different, Is.False);
+        }
+
+        [Test]
+        public void Default_port_number_ignored()
+        {
+            var left = new Uri("http://somedomain:80");
+            var right = new Uri("http://somedomain");
+
+            Assert.That(left == right, Is.True);
+        }
+
+        [Test]
+        public void Tailing_backslash_ignored()
+        {
+            var left = new Uri("http://somedomain/");
+            var right = new Uri("http://somedomain");
+
+            Assert.That(left == right, Is.True);
+        }
+
+        [Test]
+        public void Case_ignored()
+        {
+            var left = new Uri("http://SomeDomain/");
+            var right = new Uri("http://somedomain");
+
+            Assert.That(left == right, Is.True);
+        }
+    }
+}
